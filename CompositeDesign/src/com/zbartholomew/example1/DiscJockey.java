6187f030ee3ac5f68ebf9f2c84183cf07de3a5b5
zbartholomew <zach.bartholomew@gmail.com>
2017-06-28 20:52:06 -0700

    CompositeDesign

---
+++ CompositeDesign/src/com/zbartholomew/example1/DiscJockey.java
@@ -0,0 +1,18 @@
+package com.zbartholomew.example1;
+
+public class DiscJockey {
+
+    SongComponent songList;
+
+    // newSongList contains every Song, SongGroup,
+    // and any Songs saved in SongGroups
+    public DiscJockey(SongComponent newSongList) {
+        songList = newSongList;
+    }
+
+    // Calls the displaySongInfo() on every Song
+    // or SongGroup stored in songList
+    public void getSongList() {
+        songList.displaySongInfo();
+    }
+}
---
+++ CompositeDesign/src/com/zbartholomew/example2/Employee.java
@@ -0,0 +1,37 @@
+package com.zbartholomew.example2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Create Employee class having list of Employee objects.
+
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    // constructor
+    public Employee(String name, String dept, int sal) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = sal;
+        subordinates = new ArrayList<Employee>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    public String toString() {
+        return ("Employee :[ Name : " + name + ", dept : " + dept + ", salary :" + salary + " ]");
+    }
+}
