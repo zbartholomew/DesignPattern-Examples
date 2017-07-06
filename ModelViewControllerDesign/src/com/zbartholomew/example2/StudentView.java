32ea460c2642ffb387df41396b1b58aa2809fbd7
zbartholomew <zach.bartholomew@gmail.com>
2017-07-05 20:57:01 -0700

    ModelViewControllerDesign

---
+++ ModelViewControllerDesign/src/com/zbartholomew/example2/StudentView.java
@@ -0,0 +1,13 @@
+package com.zbartholomew.example2;
+
+// View
+
+public class StudentView {
+
+    public void printStudentDetails(String studentName, String studentRollNo) {
+
+        System.out.println("Student: ");
+        System.out.println("Name: " + studentName);
+        System.out.println("Roll No: " + studentRollNo);
+    }
+}
